9079227f8c73376c1beb1d64e7a762b60b80d6a4
markgreenburg <marek.greenburg@gmail.com>
2017-06-01 19:29:32 -0500

    + aligned results right + cut leading zero

--- app/js/results.js
+++ app/js/results.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const Results = (props) => {
+    let actualDisplay;
+    if (props.displayValue[0] !== "0"
+        && props.displayValue.length > 1
+        && props.displayValue[1] !== ".") {
+      actualDisplay = props.displayValue.substr(1);
+    } else {
+      actualDisplay = props.displayValue;
+    }
+    
     return (
       <div className="calculator__results">
-        {props.displayValue || '0'}
+        {actualDisplay || '0'}
       </div>
     );
 };
